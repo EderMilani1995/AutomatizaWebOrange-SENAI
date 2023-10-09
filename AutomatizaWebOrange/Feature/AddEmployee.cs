2b91e644763270660b19af54aa43a335855d1f27
EDER MILANI <EDER MILANI@MILANI>
2023-10-09 10:50:04 -0300

    Ajustes.

--- AutomatizaWebOrange/Feature/AddEmployee.cs
+++ AutomatizaWebOrange/Feature/AddEmployee.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using AutomatizaWebOrange.Steps;
+using static System.Console;
 
 namespace AutomatizaWebOrange.Feature
 {
@@ -20,6 +22,17 @@ namespace AutomatizaWebOrange.Feature
         [TestCategory("CT03")]
         public void ValidarAddEmployeeVazio()
         {
+            WriteLine("QUANDO: ACESSAR O LINK 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'");
+            WriteLine("E: PREENCHER OS CAMPOS @USERNAME E @PASSWORD COM USUÁRIO CADASTRADO"); 
+            WriteLine("QUANDO: CLICAR NO BOTÃO[LOGIN]"); 
+            WriteLine("ENTÃO: O LOGIN DEVE SER REALIZADO COM SUCESSO"); 
+            WriteLine("E: CLICAR NO MENU PIM"); 
+            WriteLine("E: CLICAR NO BOTÃO[ADD EMPLOYEE]"); 
+            WriteLine("ENTÃO: SERÁ EXIBIDA A TELA 'ADD EMPLOYEE'"); 
+            WriteLine("E: NÃO PREENCHER OS CAMPOS @FIRSTNAME, @MIDDLENAME E @LASTNAME");
+            WriteLine("QUANDO: CLICAR NO BOTÃO[SAVE]");
+            WriteLine("ENTÃO: OS CAMPOS 'FIRSTNAME' E 'LASTNAME' FICARÃO EM DESTAQUE NA COR VERMELHA E APRESENTARÃO O ALERTA 'REQUIRED'"); 
+
             AddEmployeeSteps.ValidarAddEmployeeVazio("Admin", "admin123");
         }
 
@@ -27,6 +40,18 @@ namespace AutomatizaWebOrange.Feature
         [TestCategory("CT04")]
         public void ValidarAddEmployeeFirstNameVazio()
         {
+            WriteLine("QUANDO: ACESSAR O LINK 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'"); 
+            WriteLine("E: PREENCHER OS CAMPOS @USERNAME E @PASSWORD COM USUÁRIO CADASTRADO"); 
+            WriteLine("QUANDO: CLICAR NO BOTÃO[LOGIN]"); 
+            WriteLine("ENTÃO: O LOGIN DEVE SER REALIZADO COM SUCESSO"); 
+            WriteLine("E: CLICAR NO MENU PIM"); 
+            WriteLine("E: CLICAR NO BOTÃO[ADD EMPLOYEE]"); 
+            WriteLine("ENTÃO: SERÁ EXIBIDA A TELA 'ADD EMPLOYEE'"); 
+            WriteLine("E: NÃO PREENCHER O CAMPO @FIRSTNAME"); 
+            WriteLine("E: PREENCHER O CAMPO @LASTNAME COM NOME VÁLIDO"); 
+            WriteLine("QUANDO: CLICAR NO BOTÃO[SAVE]"); 
+            WriteLine("ENTÃO: O CAMPO 'FIRSTNAME' FICARÁ EM DESTAQUE NA COR VERMELHA E APRESENTARÁ O ALERTA 'REQUIRED'"); 
+
             AddEmployeeSteps.ValidarAddEmployeeFirstNameVazio("Admin", "admin123", "Silva");
         }
 
@@ -34,6 +59,18 @@ namespace AutomatizaWebOrange.Feature
         [TestCategory("CT05")]
         public void ValidarAddEmployeeLastNameVazio()
         {
+            WriteLine("QUANDO: ACESSAR O LINK 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'"); 
+            WriteLine("E: PREENCHER OS CAMPOS @USERNAME E @PASSWORD COM USUÁRIO CADASTRADO"); 
+            WriteLine("QUANDO: CLICAR NO BOTÃO[LOGIN]"); 
+            WriteLine("ENTÃO: O LOGIN DEVE SER REALIZADO COM SUCESSO"); 
+            WriteLine("E: CLICAR NO MENU PIM"); 
+            WriteLine("E: CLICAR NO BOTÃO[ADD EMPLOYEE]"); 
+            WriteLine("ENTÃO: SERÁ EXIBIDA A TELA 'ADD EMPLOYEE'"); 
+            WriteLine("E: PREENCHER O CAMPO @FIRSTNAME COM NOME VÁLIDO"); 
+            WriteLine("E: NÃO PREENCHER O CAMPO @LASTNAME"); 
+            WriteLine("QUANDO: CLICAR NO BOTÃO[SAVE]"); 
+            WriteLine("ENTÃO: O CAMPO 'FIRSTNAME' FICARÁ EM DESTAQUE NA COR VERMELHA E APRESENTARÁ O ALERTA 'REQUIRED'"); 
+
             AddEmployeeSteps.ValidarAddEmployeeLastNameVazio("Admin", "admin123", "Cristiano");
         }
 
